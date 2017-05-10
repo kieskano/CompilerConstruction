dfe26b638d267c1c36bbe4d1092645cb2ee63c9d
Han Hollander <han_hollander@hotmail.nl>
2017-05-10 10:41:17 +0200

    kys

---
+++ src/testArrayIsobject.java
@@ -0,0 +1,13 @@
+/**
+ * Created by han on 10-5-17.
+ */
+public class testArrayIsobject {
+
+
+    public static void main(String[] args) {
+
+        int[] array = new int[3];
+
+        if (array instanceof Object) System.out.println("Yes!");
+    }
+}
